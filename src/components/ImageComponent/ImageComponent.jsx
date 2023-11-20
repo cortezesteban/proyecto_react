899dc69220c9e060c16ea281058306693a0999fe
Esteban Cortez <cortez.esteban@gmail.com>
2023-11-20 13:11:54 -0300

    feature/PreEntrega1, se agregaron imagenes grandes

--- src/components/ImageComponent/ImageComponent.jsx
+++ src/components/ImageComponent/ImageComponent.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const ImageComponent = ({juego}) => {
   return (
     <div className='imgContainerStyle'>
-        <img src={juego?.imagen} alt="" className='imgStyle'></img>
+        <img src={juego?.imagenGrande} alt="" className='imgStyle'></img>
         <Link to={`/item/${juego?.id}`} className='aImgStyle'>
           <h2 className='h2Tittle'>{juego?.nombre}</h2>
           <p>${juego?.precio}</p>
